f93e0a37f881fb8347c363cae555c666fc78112c
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-06-28 22:28:56 +0530

    add routes of react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import { Route,Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <Routes>
+    <Route path='/'/>
+    </Routes>
     </>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { assets } from '../assets/assets'
 import './Navbar.css'
+import {Link, useNavigate} from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <div className='profile'>
-      <img src={assets.profile_icon} alt='profile' />
+  const [menu,setMenu]=useState("Home");
+  return (<div className='navbar'>
+    <div className='navbar-left'>
+      <div className='profile'>
+        <img src={assets.profile_icon} alt='profile' />
+      </div>
+      <Link to="/" onClick={()=>setMenu("home")} className={menu==="Home"?"active":""}>Home</Link>
     </div>
+  </div>
+
   )
 }
 
