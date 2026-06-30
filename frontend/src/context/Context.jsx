44314a2689ac6b703f238961b65f6ece27e585d8
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-07-01 00:15:57 +0530

    add foodlist

--- frontend/src/context/Context.jsx
+++ frontend/src/context/Context.jsx
@@ -5,11 +5,20 @@ export const StoreContext=createContext(null);
 const StoreContextProvider=(props)=>{
 const [token,setToken]=useState("");
 const url="http://localhost:3000"
+const [bookList,setBookList]=useState([]);
+
+
 const contextValue={
     url,
     token,
-    setToken
+    setToken,
+    bookList
 }
+
+const fetchBookList=async()=>{
+    const response=await axios.get(`${url}/api/book/list`);
+    setBookList(response.data.data);
+  }
 return (
     <StoreContext.Provider value={contextValue}>
             {props.children}
