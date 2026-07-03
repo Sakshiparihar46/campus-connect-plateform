7a9b6a815ef8bf8c7bfa988ddea7cc4e329f1eae
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-07-03 21:35:25 +0530

    add css in adding page

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,23 +1,25 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import BookDisplay from '../components/BookDisplay'
 import { useNavigate } from 'react-router-dom';
 import {assets} from '../assets/assets.js'
 import './Books.css'
+import { StoreContext } from '../context/Context.jsx';
 function Books() {
   
   const [category,setCategory]=useState("All");
   const navigate = useNavigate();
+  const {token}=useContext(StoreContext);
   
   const handleAddClick = () => {
     navigate('/add');
   }
   
   return (
-    <div>
+    <div>{!token?<></>:
       <div className="add-option">
           <img className='add-option-img' onClick={handleAddClick} src={assets.add_icon} alt="" />
           <p className='add-option-content' onClick={handleAddClick}>Add Books</p>
-        </div>
+        </div>}
       <BookDisplay category={category}/>
     </div>
   )
