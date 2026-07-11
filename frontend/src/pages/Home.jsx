a3f1a0983ae1d7c8cadc9d9c07dd3e0e94c86d44
Sakshiparihar46 <pariharsakshi36017@gmail.com>
2026-07-12 00:24:06 +0530

    add some css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css'
 function Home() {
   return (
     <div className='home-image'>
-      <img src={assets.college_campus}/>
+      <img src={assets.college_campus} alt='Campus overview' />
     </div>
   )
 }
